756286b956381722c2032170b6008f78e8a1bf4e
Jaime Broseman <Jaime Broseman>
2016-07-09 00:18:32 -0700

    Create Logger class (untested)

---
+++ src/Logger.php
@@ -0,0 +1,113 @@
+<?php
+
+class Logger
+{
+	private $logfilepath = '../logs/dashboard-log.txt';
+
+	public function __construct()
+	{
+		$this->SetLogFile();
+	}
+
+	private function SetLogFile()
+	{
+        if (!$logfile = fopen($logfilepath, "r")) 
+        {
+            echo "Cannot open file ($logfile)";
+            exit;
+        }
+
+        $filestart = fread($logfile, 3);
+
+        if (isset($filestart) && $filestart !== date('m-d-y'))
+        {
+            fclose($logfile);
+            rename($logfilepath, "../logs/dashboard-log_$today.txt");
+        }
+	}
+
+	public function WriteLog($level, $source, $text)
+	{
+        if (!$logfile = fopen($logfilepath, "a+")) 
+        {
+            echo "Cannot open file ($logfilepath)";
+            exit;
+        }
+
+        $source = str_pad($source, 10, " ");
+        $timestamp = NOW();
+        $entry = "$timestamp $level $source - $text\n";
+        
+        if (fwrite($logfile, $entry) === FALSE) {
+            echo "Cannot write to file ($logfilepath)";
+            exit;
+        }
+
+        echo "Success, wrote ($entry) to file ($logfilepath)";
+
+        fclose($logfile);
+	}
+
+	// public function GetBlinds($gameID)
+	// {
+	// 	$sql = '
+	//     	SELECT 	b.BlindID,
+	//     			SmallBlind,
+	//     			LargeBlind,
+	//     			ChipUpID,
+	// 				(SELECT ImageFilename FROM chips WHERE ChipID = ChipUpID) AS ChipUpIMG,
+	//     			EndOfRebuy,
+	// 				(SELECT EXISTS( SELECT * FROM completedblinds cb WHERE cb.BlindID = b.BlindID and cb.GameID = :gameid)) AS Completed
+	//     	FROM 	blinds b
+	//     	WHERE 	Status > 0
+	// 	';
+
+	// 	$statement = $this->database->prepare($sql);
+	// 	$statement->bindValue(':gameid', $gameID);
+	// 	$statement->execute();
+	// 	return $statement->fetchAll(PDO::FETCH_ASSOC);		
+	// }
+
+	// function ClearSavedTime($data)
+	// {
+	// 	$sql = '
+	// 		UPDATE	timervalues
+	// 		SET		Status = 0
+	// 		WHERE	Status = 1
+	// 		AND		GameID = :gameid
+	// 	';
+
+	//     $statement = $this->database->prepare($sql);
+	// 	$statement->bindValue(':gameid', $data['gameid']);
+
+	// 	$result = array(
+    //         "success" => $statement->execute(),
+    //         "message" => $statement->errorCode()
+	//     );
+
+	// 	json_encode($result);
+	// }
+	
+	// function UpsertEndBlind($blind)
+	// {
+    //     $sql = '
+    //         INSERT INTO completedblinds 
+    //             (BlindID, 
+    //             GameID)
+    //         VALUES 
+    //             (:blindid,
+    //             :gameid)
+    //         ';
+
+	//     $statement = $this->database->prepare($sql);
+	// 	$statement->bindValue(':blindid', $blind['BlindID']);
+	// 	$statement->bindValue(':gameid', $blind['GameID']);
+
+	// 	$result = array(
+    //         "success" => $statement->execute(),
+    //         "message" => $statement->errorCode()
+	//     );
+	// }
+}
+
+?>
